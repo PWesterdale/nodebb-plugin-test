22f68ba165b40c06e853c1dfb13b637b4d9b79cc
Paul Westerdale <paul@westerdale.me>
2016-04-27 09:10:32 +0100

    Are plugins strict?

--- library.js
+++ library.js
@@ -10,7 +10,7 @@ AppInsights.post = function(type, data) {
 	}, function(err, res, body){
 		console.log(body);
 	});
-}
+};
 
 AppInsights.topicSave = function(data) {
 	this.post('new topic', data);
@@ -22,19 +22,19 @@ AppInsights.postSave = function(data) {
 
 AppInsights.postUpVote = function(data) {
 	this.post('new upvote', data);
-}
+};
 
 AppInsights.postDownVote = function(data) {
 	this.post('new downvote', data);
-}
+};
 
 AppInsights.postUnvote = function(data) {
 	this.post('remove vote', data);
-}
+};
 
 AppInsights.chatMessage = function(data, cb) {
 	this.post('chat message!', data);
 	cb();
-}
+};
 
 module.exports = AppInsights;
